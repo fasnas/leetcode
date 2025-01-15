9e5f9a101c74b3918ea5a23b59387aac750ef374
fasnas <103342049+fasnas@users.noreply.github.com>
2025-01-15 09:38:02 +0530

    Time: 0 ms (100%), Space: 48 MB (97.98%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    let a=[]
+    a.push(celsius+273.15)
+    a.push(celsius*1.80+32)
+    return a
+};
