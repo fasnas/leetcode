1b373a4d50c688b0cf03b1ab04ffdbf687092410
Muhammad Fasnas <103342049+fasnas@users.noreply.github.com>
2025-02-11 09:35:39 +0530

    Time: 0 ms (100%), Space: 55.9 MB (75.85%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,21 +3,6 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let left = 0;
-    let right = s.length - 1;
-    
-    while (left < right) {
-        // Swap characters
-        let temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        
-        left++;
-        right--;
-    }
+    return s.reverse()
 };
 
-// Example usage:
-let str = ["h","e","l","l","o"];
-reverseString(str);
-console.log(str); // ["o","l","l","e","h"]
