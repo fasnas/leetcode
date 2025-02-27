3da129bb885e4fa345c1684fbe28a58a8e5c97c1
Muhammad Fasnas <103342049+fasnas@users.noreply.github.com>
2025-02-27 09:27:38 +0530

    Time: 0 ms (100%), Space: 53.5 MB (28.41%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+   
+    let a= BigInt(num1)*BigInt(num2)
+     return a.toString()
+};
