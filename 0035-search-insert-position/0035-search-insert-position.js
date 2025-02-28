553b0a7a709552eca84b7629466db60eefdd91dc
Muhammad Fasnas <103342049+fasnas@users.noreply.github.com>
2025-02-28 10:01:32 +0530

    Time: 0 ms (100%), Space: 54.7 MB (5.44%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,8 +4,11 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  nums.push(target)
-  let y=nums.sort((a,b)=>a-b)
-  let z=y.indexOf(target)
-  return z
+    
+    for(i=0;i<nums.length;i++){
+        if(target<=nums[i]){
+            return i
+        }
+    }
+     return nums.length  
 };
