9c53ab250c702f67ca2e1cecba2036eb892a39d5
Muhammad Fasnas <103342049+fasnas@users.noreply.github.com>
2025-04-19 12:55:54 +0530

    Time: 0 ms (100%), Space: 53.9 MB (63.3%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+   return stones.split("").filter((x)=>jewels.includes(x)).length
+
+};
