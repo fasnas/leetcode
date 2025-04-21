e6c01e44e84010a0dd0b359cc53cef520238a072
Muhammad Fasnas <103342049+fasnas@users.noreply.github.com>
2025-04-21 10:04:03 +0530

    Time: 0 ms (100%), Space: 54.3 MB (75.58%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let res=[-1,-1]
+     res[0]=nums.indexOf(target)
+     res[1]=nums.lastIndexOf(target)
+    return  res;
+};
