3595ef93d0c525738d784c64e795f12110e420f7
Muhammad Fasnas <103342049+fasnas@users.noreply.github.com>
2025-06-14 10:09:24 +0530

    Time: 1 ms (82.08%), Space: 54.1 MB (77.33%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -7,6 +7,6 @@ var intersection = function(nums1, nums2) {
    let a=nums1.filter((x)=>{
      return nums2.includes(x)
    })
-   let res=[ ...new Set([...a])]
+   let res=[...new Set(a)]
    return res
 };
